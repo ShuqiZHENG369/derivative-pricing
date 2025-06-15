7b61a63f61d50bf5096d7ca3594c158ae16fcc59
Shuqi ZHENG <162972715+ShuqiZHENG369@users.noreply.github.com>
2025-06-15 14:39:21 +0200

    Update implied_vol.py

--- implied_vol.py
+++ implied_vol.py
@@ -2,7 +2,7 @@
 # implied_vol.py
 
 from scipy.optimize import fsolve
-from bsm_model import bsm_call_value, bsm_put_value
+from bsm_model import bsm_call_price, bsm_put_price
 
 def compute_implied_vol(price, spot, strike, maturity, rate, dividend_yield=0.0, 
                         option_type='call', initial_vol=0.2):
@@ -35,9 +35,9 @@ def compute_implied_vol(price, spot, strike, maturity, rate, dividend_yield=0.0,
     """
     def objective(vol):
         if option_type == 'call':
-            return bsm_call_value(spot, strike, maturity, rate, vol, dividend_yield) - price
+            return bsm_call_price(spot, strike, maturity, rate, vol, dividend_yield) - price
         elif option_type == 'put':
-            return bsm_put_value(spot, strike, maturity, rate, vol, dividend_yield) - price
+            return bsm_put_price(spot, strike, maturity, rate, vol, dividend_yield) - price
         else:
             raise ValueError("option_type must be 'call' or 'put'.")
 
