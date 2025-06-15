0a7c23dde2789772133e2a0b1f05fce1a3d56260
Shuqi ZHENG <162972715+ShuqiZHENG369@users.noreply.github.com>
2025-06-15 14:52:40 +0200

    Add files via upload

---
+++ greeks.py
@@ -0,0 +1,25 @@
+# greeks.py
+
+def compute_greeks(model, option_type='call'):
+    """
+    Compute all Greeks for a given BSM model instance.
+
+    Parameters
+    ----------
+    model : BlackScholesModel
+        An instance of the Black-Scholes-Merton model.
+    option_type : str
+        'call' or 'put'
+
+    Returns
+    -------
+    dict
+        A dictionary of Delta, Gamma, Vega, Theta, Rho.
+    """
+    return {
+        'delta': model.bsm_delta(option_type),
+        'gamma': model.bsm_gamma(),
+        'vega': model.bsm_vega(),
+        'theta': model.bsm_theta(option_type),
+        'rho': model.bsm_rho(option_type)
+    }
